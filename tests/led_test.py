1bc1008eecfa90f21c7d4d7d64e6c6841015a0f7
Mehek@Rutalab <rutalabflymanager@gmail.com>
2026-01-14 12:47:25 -0500

    Enhance GPU test notebook and LED test script
    - Updated gpu_test.ipynb to include loading of rig configuration from JSON and saving to HDF5 format with compression for large arrays.
    - Added error handling for missing 'basler_camera' module in gpu_test.ipynb.
    - Refactored led_test.py to implement a continuous rolling LED pattern effect with OpenCV integration for real-time visualization.
    - Included exception handling for graceful shutdown of the LED controller connection.

--- tests/led_test.py
+++ tests/led_test.py
@@ -1,18 +1,39 @@
 import time
 from flyprojection.controllers.led_server import WS2812B_LEDController
 import matplotlib.pyplot as plt
+import cv2
+import numpy as np
 
 if __name__ == "__main__":
-    with WS2812B_LEDController(host='flyprojection-server', port=65432) as server:
-        # Send a solid color image
-        color_image = server.create_radial(
-        center=(32, 3),
-        ring_width=2,
-        color1=(0, 0, 255),
-        color2=(0, 0, 0),
-        )
-        # show image in matplotlib
-        plt.imshow(color_image)
-        plt.show()
-        server.send_image(color_image)
-        time.sleep(120)
+
+    with WS2812B_LEDController(host="flyprojection-server", port=65432) as server:
+        try:
+            # Send a solid color image
+            color_image = server.create_radial(
+                center=(32, 3),
+                ring_width=2,
+                color1=(0, 0, 50),
+                color2=(0, 0, 0),
+            )
+            # create window to display the image using OpenCV
+            # cv2.namedWindow("LED Pattern", cv2.WINDOW_NORMAL)
+            # cv2.resizeWindow("LED Pattern", 800, 600)
+            # cv2.imshow("LED Pattern", color_image.astype("uint8"))
+            # cv2.waitKey(1)
+            while True:
+                # roll the image to create a moving effect
+                color_image = np.roll(color_image, shift=1, axis=0)
+                # change color
+                color_image = np.roll(color_image, shift=1, axis=2)
+                server.send_image(color_image)
+                # cv2.imshow("LED Pattern", color_image.astype("uint8"))
+                # cv2.waitKey(1)
+                time.sleep(0.1)
+        except KeyboardInterrupt:
+            print("Interrupted by user. Exiting...")
+        finally:
+            # cv2.destroyAllWindows()
+            server.close_connection()
+            print(
+                "Successfully closed LED controller connection. Exiting WS2812B_LEDController Context."
+            )
